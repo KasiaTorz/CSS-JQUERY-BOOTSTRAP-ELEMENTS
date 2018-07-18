f5df30c5b4af136e90b35f273f69ce86b5b07233
KasiaTorz <ktorz@wp.pl>
2018-07-18 14:05:37 +0200

    add jquery function to action-next-first button

--- PROJECT 2/script.js
+++ PROJECT 2/script.js
@@ -52,6 +52,34 @@ next_fs.show();
       });
 
 });
+$(".action-next-first").click(function(){
+      current_fs= $(this).parent();
+      next_fs= $(this).parent().next();
+//active next step in progressbar using button next//
+$("#progressbar li- form active").eq($("fieldset").index(next_fs)).addClass("active");
+
+// show next fieldset//
+next_fs.show();
+ //next fieldset will be hide //
+      current_fs.animate({opacity:0}, {
+            step:function(now,mx){
+                  // scale current_fs from the right//
+                  scale=1-(1-now)*0.2;
+                  // bring next_fs form right//
+                  left= (now*50)+"%";
+                  //increase opacity to next_fs //
+                  opacity= 1-now;
+                  current_fs.css({'transform':'scale(+scale+)'});
+                  next_fs.css({'left':left, 'opacity':opacity});
+            },
+            duration:800,
+            complete:function(){
+                  current_fs.hide();
+            },
+            easing:'easeInOutBack'
+      });
+
+});
 $(".action-previous").click(function(){
       current_fs= $(this).parent();
       previous_fs= $(this).parent().prev();
