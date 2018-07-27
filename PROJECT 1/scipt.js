f7376a2e8ed0ad47bd83f1e23ded315450e3485b
KasiaTorz <ktorz@wp.pl>
2018-07-27 14:19:08 +0200

    add function to every color a

--- PROJECT 1/scipt.js
+++ PROJECT 1/scipt.js
@@ -1,3 +1,16 @@
-function changecolor(id){
-document.body.style.background= document.getElementById("id").innerHTML;
-};
+function changeColorRed(newColor){
+    
+document.body.style.background= "red";
+}
+function changeColorBlue(newColor){
+    document.body.style.background= "blue";
+}
+function changeColorGreen(newColor){
+    document.body.style.background= "gren";
+}
+function changeColorGray(newColor){
+    document.body.style.background= "gray";
+}
+function changeColorYellow(newColor){
+    document.body.style.background= "yellow";
+}
