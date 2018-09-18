62c056319d9c8a54487f9cee9c9a5bbe6d7cc1b1
KasiaTorz <ktorz@wp.pl>
2018-09-18 14:13:40 +0200

    add magnific object in js

--- PROJECT 3/js/app.js
+++ PROJECT 3/js/app.js
@@ -2,3 +2,11 @@
 $(function(){
     new WOW().init();  /*to scroll and have up service*/
 });
+
+/*--PORTFOLIO---*/
+$(function (){
+  $("#work").magnificPopup({
+    delegate: 'a',
+    type: 'image'
+  });
+});
