4d49ea5012a8cca2695f62dcd8d7deabab364534
KasiaTorz <ktorz@wp.pl>
2018-09-19 14:44:52 +0200

    remove magnific jquery in app js

--- PROJECT 3/js/app.js
+++ PROJECT 3/js/app.js
@@ -8,9 +8,3 @@ $(function(){
     new WOW().init();  /*to scroll and have up service*/
 });
 
-/*-----------Magnific pop--- */
-$('#work').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image'
-    // other options
-  });
