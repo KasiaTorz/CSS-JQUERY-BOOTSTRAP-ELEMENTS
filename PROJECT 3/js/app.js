3524332ef3e233c28dc6a1177b30b4f3ee735ec6
KasiaTorz <ktorz@wp.pl>
2018-12-19 17:09:41 +0100

    add onclick close navbar  event in jquery function

--- PROJECT 3/js/app.js
+++ PROJECT 3/js/app.js
@@ -80,6 +80,6 @@ $ (function(){
 /*-------close mobile menu onclick----------*/
 $(function(){
     $(".navbar-collapse ul li a").on("click touch",function(){
-
+        $(".navbar-toggle").click();
     });
 });
